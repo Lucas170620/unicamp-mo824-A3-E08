e0abb002ff988aea3a1ad3510efb696ebff1f467
Eliel <e295745@dac.unicamp.br>
2025-09-19 23:07:12 -0300

    add tabu search framework

---
+++ TS-Framework/src/problems/qbf/MaxSCQBF.java
@@ -0,0 +1,130 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+package problems.qbf;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StreamTokenizer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import solutions.Solution;
+
+public class MaxSCQBF extends QBF_Inverse {
+   public ArrayList<Integer>[] S;
+   public int[] coverCount;
+   public int n;
+   private boolean[][] coversBit;
+
+   public MaxSCQBF(String filename) throws IOException {
+      super(filename);
+      this.coverCount = new int[this.n];
+      this.coversBit = new boolean[this.n][this.n];
+
+      int k;
+      for(int i = 0; i < this.n; ++i) {
+         for(Iterator var4 = this.S[i].iterator(); var4.hasNext(); this.coversBit[i][k] = true) {
+            k = (Integer)var4.next();
+         }
+      }
+
+   }
+
+   @Override
+   protected Integer readInput(String filename) throws IOException {
+       try (Reader r = new BufferedReader(new FileReader(filename))) {
+           StreamTokenizer st = new StreamTokenizer(r);
+           st.parseNumbers();
+
+           // n
+           st.nextToken();
+           this.n = (int) st.nval;
+
+           // aloca estruturas
+           this.A = new Double[this.n][this.n];
+           @SuppressWarnings("unchecked")
+           ArrayList<Integer>[] _S = new ArrayList[this.n];
+           this.S = _S;
+           int[] sizes = new int[this.n];
+
+           // tamanhos de cada S[i]
+           for (int i = 0; i < this.n; i++) {
+               st.nextToken();
+               sizes[i] = (int) st.nval;
+           }
+
+           // elementos de cada S[i]
+           for (int i = 0; i < this.n; i++) {
+               this.S[i] = new ArrayList<>(sizes[i]);
+               for (int j = 0; j < sizes[i]; j++) {
+                   st.nextToken();
+                   int k = (int) st.nval;
+                   if (k < 0 || k >= this.n) {
+                       throw new IOException("Elemento de S[" + i + "] fora de [0," + (this.n - 1) + "]: k=" + k);
+                   }
+                   this.S[i].add(k);
+               }
+           }
+
+           // matriz A (triangular superior); abaixo da diagonal = 0.0
+           for (int i = 0; i < this.n; i++) {
+               for (int j = i; j < this.n; j++) {
+                   st.nextToken();
+                   this.A[i][j] = st.nval;
+                   if (j > i) this.A[j][i] = 0.0;
+               }
+           }
+
+           return this.n;
+       }
+   }
+
+
+   public void resetCoverage() {
+      Arrays.fill(this.coverCount, 0);
+   }
+
+   public void applySolutionToCoverage(Solution<Integer> sol) {
+      this.resetCoverage();
+      Iterator var3 = sol.iterator();
+
+      while(var3.hasNext()) {
+         int i = (Integer)var3.next();
+
+         int var10002;
+         int k;
+         for(Iterator var5 = this.S[i].iterator(); var5.hasNext(); var10002 = this.coverCount[k]++) {
+            k = (Integer)var5.next();
+         }
+      }
+
+   }
+
+   public boolean removalBreaksCoverage(int i) {
+      Iterator var3 = this.S[i].iterator();
+
+      while(var3.hasNext()) {
+         int k = (Integer)var3.next();
+         if (this.coverCount[k] == 1) {
+            return true;
+         }
+      }
+
+      return false;
+   }
+
+   public boolean covers(int i, int k) {
+      return this.coversBit[i][k];
+   }
+
+   public boolean isFeasible() {
+      for(int k = 0; k < this.n; ++k) {
+         if (this.coverCount[k] <= 0) {
+            return false;
+         }
+      }
+
+      return true;
+   }
+}
---
+++ TS-Framework/src/problems/qbf/solvers/TS_MAXSCQBF.java
@@ -0,0 +1,182 @@
+package problems.qbf.solvers;
+
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+
+import metaheuristics.tabusearch.AbstractTS;
+import problems.qbf.MaxSCQBF;
+import solutions.Solution;
+
+
+public class TS_MAXSCQBF extends AbstractTS<Integer> {
+
+    private final Integer fake = Integer.valueOf(-1);
+    private final MaxSCQBF eval;
+
+    public TS_MAXSCQBF(Integer tenure, Integer iterations, String filename) throws IOException {
+        super(new MaxSCQBF(filename), tenure, iterations);
+        this.eval = (MaxSCQBF) this.ObjFunction;
+    }
+
+    @Override
+    public ArrayList<Integer> makeCL() {
+        ArrayList<Integer> _CL = new ArrayList<>();
+        for (int i = 0; i < ObjFunction.getDomainSize(); i++) 
+        	_CL.add(i);
+        return _CL;
+    }
+
+    @Override
+    public ArrayList<Integer> makeRCL() { 
+    	return new ArrayList<>(); 
+    }
+
+    @Override
+    public ArrayDeque<Integer> makeTL() {
+        ArrayDeque<Integer> _TS = new ArrayDeque<>(2 * tenure);
+        for (int i = 0; i < 2 * tenure; i++) 
+        	_TS.add(fake);
+        return _TS;
+    }
+
+    @Override
+    public void updateCL() {
+        // sem heurística adicional por enquanto
+    }
+
+    @Override
+    public Solution<Integer> createEmptySol() {
+        Solution<Integer> sol = new Solution<>();
+        sol.cost = 0.0;
+        eval.resetCoverage();
+        return sol;
+    }
+
+    @Override
+    public Solution<Integer> neighborhoodMove() {
+        // 0) garantir contadores de cobertura consistentes
+        eval.applySolutionToCoverage(sol);
+
+        // 0.1) se ainda não é factível, inserir UM conjunto que cubra algo descoberto e retornar
+        if (!eval.isFeasible()) {
+            int uncovered = -1;
+            for (int k = 0; k < eval.n; k++) {
+                if (eval.coverCount[k] == 0) { 
+                	uncovered = k; break; 
+                }
+            }
+            if (uncovered >= 0) {
+                Integer bestCand = null;
+                double bestDelta = Double.POSITIVE_INFINITY;
+                for (Integer candIn : CL) {
+                    if (eval.covers(candIn, uncovered)) {
+                        double d = ObjFunction.evaluateInsertionCost(candIn, sol);
+                        if (d < bestDelta) { bestDelta = d; bestCand = candIn; }
+                    }
+                }
+                if (bestCand != null) {
+                    TL.poll(); 
+                    TL.add(fake);
+                    TL.poll(); 
+                    TL.add(bestCand);
+                    sol.add(bestCand);
+                    for (Integer k : eval.S[bestCand]) eval.coverCount[k]++;
+                    CL.remove(bestCand);
+                    ObjFunction.evaluate(sol);
+                }
+            }
+            return sol;
+        }
+
+        // 1) busca local padrão (ADD, DROP, SWAP) com cobertura respeitada
+        Double minDeltaCost = Double.POSITIVE_INFINITY;
+        Integer bestCandIn = null, bestCandOut = null;
+
+        updateCL();
+
+        // 1.1) Avalia insertions
+        for (Integer candIn : CL) {
+            Double deltaCost = ObjFunction.evaluateInsertionCost(candIn, sol);
+            if (!TL.contains(candIn) || sol.cost + deltaCost < bestSol.cost) {
+                if (deltaCost < minDeltaCost) {
+                    minDeltaCost = deltaCost;
+                    bestCandIn = candIn;
+                    bestCandOut = null;
+                }
+            }
+        }
+
+        // 1.2) Avalia removals (somente se não quebra cobertura)
+        for (Integer candOut : sol) {
+            if (eval.removalBreaksCoverage(candOut)) 
+            	continue;
+            Double deltaCost = ObjFunction.evaluateRemovalCost(candOut, sol);
+            if (!TL.contains(candOut) || sol.cost + deltaCost < bestSol.cost) {
+                if (deltaCost < minDeltaCost) {
+                    minDeltaCost = deltaCost;
+                    bestCandIn = null;
+                    bestCandOut = candOut;
+                }
+            }
+        }
+
+        // 1.3) Avalia exchanges (somente se o swap preservar cobertura)
+        for (Integer candIn : CL) {
+            for (Integer candOut : sol) {
+                if (!swapPreservaCobertura(candOut, candIn)) 
+                	continue;
+                Double deltaCost = ObjFunction.evaluateExchangeCost(candIn, candOut, sol);
+                if ((!TL.contains(candIn) && !TL.contains(candOut)) || sol.cost + deltaCost < bestSol.cost) {
+                    if (deltaCost < minDeltaCost) {
+                        minDeltaCost = deltaCost;
+                        bestCandIn = candIn;
+                        bestCandOut = candOut;
+                    }
+                }
+            }
+        }
+
+        // 2) Aplica o melhor movimento não-tabu (ou tabu com aspiração)
+        TL.poll();
+        if (bestCandOut != null) {
+            sol.remove(bestCandOut);
+            for (Integer k : eval.S[bestCandOut]) 
+            	eval.coverCount[k]--;
+            CL.add(bestCandOut);
+            TL.add(bestCandOut);
+        } else {
+            TL.add(fake);
+        }
+
+        TL.poll();
+        if (bestCandIn != null) {
+            sol.add(bestCandIn);
+            for (Integer k : eval.S[bestCandIn]) eval.coverCount[k]++;
+            CL.remove(bestCandIn);
+            TL.add(bestCandIn);
+        } else {
+            TL.add(fake);
+        }
+
+        ObjFunction.evaluate(sol);
+        return sol;
+    }
+
+    private boolean swapPreservaCobertura(int out, int in) {
+        for (Integer k : eval.S[out]) {
+            if (eval.coverCount[k] == 1 && !eval.covers(in, k)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        String inst = (args.length > 0) ? args[0] : "instances/max-sc-qbf/max_sc_qbf-n_200-k_3.txt";
+        TS_MAXSCQBF tabusearch = new TS_MAXSCQBF(20, 1000, inst);
+        Solution<Integer> bestSol = tabusearch.solve();
+        System.out.println("Best = " + bestSol + " | max = " + (-bestSol.cost));
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time = " + (endTime - startTime) / 1000.0 + " s");
+    }
+}
